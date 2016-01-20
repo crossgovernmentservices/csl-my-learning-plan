11fb3f0bbb8bab0ffd399dc06b423ae6452ade9d
Chris Duraj <christopherduraj@gmail.com>
2016-01-20 09:48:21 +0000

    Add configuration for LRS

--- application/config/config.py
+++ application/config/config.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, json
 
-
 class Config(object):
     APP_ROOT = os.path.abspath(os.path.dirname(__file__))
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_ROOT, os.pardir))
@@ -16,6 +15,14 @@ class Config(object):
     MONGODB_SETTINGS = {
         'host': os.environ.get('MONGO_URI')
     }
+
+    LRS_HOST = os.environ.get('LRS_HOST')
+    LRS_PORT = os.environ.get('LRS_PORT')
+    LRS_HTTPS_ENABLED = os.environ.get('LRS_HTTPS_ENABLED')
+    LRS_USER = os.environ.get('LRS_USER')
+    LRS_PASS = os.environ.get('LRS_PASS')
+
+    LRS_QUERY_URL = os.environ.get('LRS_QUERY_URL')
 
 class DevelopmentConfig(Config):
     DEBUG = True
