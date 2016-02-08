bda306d147115bde53e0a77b663731d8254c401c
cmorandi <kryzm@yahoo.com>
2016-02-08 11:02:04 +0000

    added free, professions filters

--- application/modules/lr_service.py
+++ application/modules/lr_service.py
@@ -14,9 +14,6 @@ def get_resources():
     return [i['resource_data_description']['resource_data'] for i in items['documents']]
 
 def get_all_courses_from_learning_registry():
-    import urllib.request
-    response = urllib.request.urlopen(LEARNING_REGISTRY_SANDBOX)
-    items = json.loads(response.read().decode('utf-8'))
     def convert_item(item):
         def map_type(resource_data):
             type_switcher = {
@@ -31,16 +28,20 @@ def get_all_courses_from_learning_registry():
             logger.debug('Type is ' + resource_data['@type'])
             return type_switcher.get(resource_data.get("@type","undefined"),"undefined")
 
-        converted =  { 'title' : item['resource_data_description']['resource_data']['name'],
-                 'type' :  map_type(item['resource_data_description']['resource_data']),
-                 'desc' :  item['resource_data_description']['resource_data']['description'],
-                 'url' :  item['resource_data_description']['resource_data']['url'], 
-                 'topics' : item['resource_data_description']['resource_data']['keywords']}
-        if 'timeRequired' in item['resource_data_description']['resource_data']:
-            converted['duration'] = item['resource_data_description']['resource_data']['timeRequired']
+        converted =  { 'title' : item['name'],
+                'audience' : item['audience'],
+                 'type' :  map_type(item),
+                 'desc' :  item['description'],
+                 'url' :  item['url'], 
+                 'price' : item['offers']['price'] + item['offers']['priceCurrency']  if 'offers' in item else 'Free Resource',
+                 'topics' : item['keywords']
+                 }
+
+        if 'timeRequired' in item:
+            converted['duration'] = item['timeRequired']
         return converted
 
-    return [convert_item(i) for i in items['documents']]
+    return [convert_item(i) for i in get_resources()]
 
 def get_all_courses():
     with open(DATA_FILEPATH) as data_file:
@@ -53,8 +54,13 @@ def get_courses(filterDict):
         return all_courses
     mainFilter = filterDict["filter"].get("main-search", '').lower()
     typeFilter = filterDict["filter"].get("type-filter", '').lower()
+    professionFilter = filterDict["filter"].get("profession-filter", '').lower()
+    freeResourcesOnly = filterDict["filter"].get("free-resources-only", 'False')
+    logger.debug('freeResourcesOnly ' + freeResourcesOnly)
     return [each for each in all_courses
                 if (typeFilter is '' or typeFilter in each.get('type', ''))
+                and (freeResourcesOnly == 'False' or 'Free' in each.get('price', 'Free'))
+                and (professionFilter is '' or professionFilter in each.get('audience', '').lower())
                 and (mainFilter in each.get('title','').lower()
                         or mainFilter in each.get('desc','').lower()
                         or [eachTopic for eachTopic in each.get('topics', []) if mainFilter in eachTopic.lower()])
