2582d8fcc7bae20f47c63ece47ba090b139e00ba
cmorandi <kryzm@yahoo.com>
2016-02-10 10:55:03 +0000

    fixed date formatting

--- application/recommendationrules/learning_registry_match.py
+++ application/recommendationrules/learning_registry_match.py
@@ -61,15 +61,23 @@ def __matchitem(item, matchingitems):
             return matchitem
     return None
 
+def __formatdurationunit(item, unitlabel):
+    item = int(item)
+    if item == 0:
+        return ''
+    if item > 1:
+        unitlabel = unitlabel + 's'
+    return str(item) + ' ' + unitlabel + ' ' 
+
 def __convertduration(duration):
     match = re.search('(-)?P(?:([\.,\d]+)Y)?(?:([\.,\d]+)M)?(?:([\.,\d]+)W)?(?:([\.,\d]+)D)?(?:T)?(?:([\.,\d]+)H)?(?:([\.,\d]+)M)?(?:([\.,\d]+)S)?', duration)
     return (match.group(2) + ' Years'    if match.group(2) is not None else '') + \
-                (match.group(3) + ' Months ' if match.group(3) is not None else  '')  + \
-                (match.group(4) + ' Weeks ' if match.group(4) is not None else  '') + \
-                (match.group(5) + ' Days ' if match.group(5) is not None else  '') + \
-                (match.group(6) + ' Hours ' if match.group(6) is not None else  '') + \
-                (match.group(7) + ' Minutes ' if match.group(7) is not None else  '') + \
-                (match.group(8) + ' Seconds' if match.group(8) is not None else  '')
+                (__formatdurationunit(match.group(3), 'Month') if match.group(3) is not None else  '')  + \
+                (__formatdurationunit(match.group(4), 'Week') if match.group(4) is not None else  '') + \
+                (__formatdurationunit(match.group(5), 'Day') if match.group(5) is not None else  '') + \
+                (__formatdurationunit(match.group(6), 'Hour') if match.group(6) is not None else  '') + \
+                (__formatdurationunit(match.group(7), 'Minute') if match.group(7) is not None else  '') + \
+                (__formatdurationunit(match.group(8), 'Second') if match.group(8) is not None else  '')
 
 
 def run(matchingitems, candidate_data_generator):
